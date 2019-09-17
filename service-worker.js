6c7b12c61b7499eed88ae6cf8e88b97cb4fcf010
Kate Hunt <katehunt789@gmail.com>
2019-09-17 15:33:35 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/keighthunt/katehuntapp/precache-manifest.471dd76b933bc97716ea50766b2465af.js"
+  "/precache-manifest.17175ba68f7c0505007f839a961248d4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/keighthunt/katehuntapp/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
